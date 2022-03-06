e408b0795a187f75c7aebac16a93e273caac107b
weronikauyar <uyarweronika@gmail.com>
2022-03-07 00:21:54 +0100

    ..
    ..

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
- // When the user clicks on the button,
- // toggle between hiding and showing the dropdown content
-
- function showPopup() {
-     event.preventDefault();
-     document.getElementById("popup_content").classList.toggle("show");
-     return false;
- }
-
- // Close the dropdown if the user clicks outside of it
-
- window.onclick = function(event) {
-     if (!event.target.matches('.thePopup')) {
-
-         var dropdowns = document.getElementsByClassName("popup_content");
-         var i;
-         for (i = 0; i < dropdowns.length; i++) {
-             var openDropdown = dropdowns[i];
-             if (openDropdown.classList.contains('show')) {
-                 openDropdown.classList.remove('show');
-             }
-         }
-     }
- }
- 
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
