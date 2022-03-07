cd7ef6df09bc2bf55b1d258d90bb8a5114a852a5
weronikauyar <uyarweronika@gmail.com>
2022-03-07 12:37:55 +0100

    ..

--- script.js
+++ script.js
@@ -43,3 +43,58 @@ function myFunction9() {
   var popup = document.getElementById("myPopup9");
   popup.classList.toggle("show");
 }
+
+function myFunction10() {
+  var popup = document.getElementById("myPopup10");
+  popup.classList.toggle("show");
+}
+
+function myFunction11() {
+  var popup = document.getElementById("myPopup11");
+  popup.classList.toggle("show");
+}
+
+function myFunction12() {
+  var popup = document.getElementById("myPopup12");
+  popup.classList.toggle("show");
+}
+
+function myFunction13() {
+  var popup = document.getElementById("myPopup13");
+  popup.classList.toggle("show");
+}
+
+function myFunction14() {
+  var popup = document.getElementById("myPopup14");
+  popup.classList.toggle("show");
+}
+
+function myFunction15() {
+  var popup = document.getElementById("myPopup15");
+  popup.classList.toggle("show");
+}
+
+function myFunction16() {
+  var popup = document.getElementById("myPopup16");
+  popup.classList.toggle("show");
+}
+
+function myFunction17() {
+  var popup = document.getElementById("myPopup17");
+  popup.classList.toggle("show");
+}
+
+function myFunction18() {
+  var popup = document.getElementById("myPopup18");
+  popup.classList.toggle("show");
+}
+
+function myFunction19() {
+  var popup = document.getElementById("myPopup19");
+  popup.classList.toggle("show");
+}
+
+function myFunction20() {
+  var popup = document.getElementById("myPopup20");
+  popup.classList.toggle("show");
+}
